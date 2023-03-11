bb145b721127d09e1be18eb76e57d4659753f804
Lei-0513 <minlei@hiretual.com>
2023-03-11 21:53:18 +0800

    set

--- index.js
+++ index.js
@@ -1,38 +1,61 @@
-function* task() {
-    const d = yield 1;
-    // //d : 1
-    const resp = yield fetch("http://study.yuanjin.tech/api/local")
-    const result = yield resp.json();
-    console.log(result);
-}
-
-run(task)
-
-function run(generatorFunc) {
-    const generator = generatorFunc();
-    let result = generator.next(); //启动任务（开始迭代）, 得到迭代数据
-    console.log('result', result);
-    handleResult();
-    //对result进行处理
-    function handleResult() {
-        if (result.done) {
-            return; //迭代完成，不处理
-        }
-        //迭代没有完成，分为两种情况
-        //1. 迭代的数据是一个Promise
-        //2. 迭代的数据是其他数据
-        if (typeof result.value.then === "function") {
-            //1. 迭代的数据是一个Promise
-            //等待Promise完成后，再进行下一次迭代
-            result.value.then(data => {
-                result = generator.next(data)
-                handleResult();
-            })
-        } else {
-            //2. 迭代的数据是其他数据，直接进行下一次迭代
-            result = generator.next(result.value)
-            handleResult();
-        }
+class MySets {
+  constructor(int = []) {
+    this._datas = [];
+    if (typeof int[Symbol.iterator] === "function") {
+      for (const item of int) {
+        this.add(item);
+      }
+    } else {
+      throw Error(`${int} is not iterator`);
     }
+  }
+
+  add(addInfo) {
+    if (!this.has(addInfo)) {
+      this._datas.push(addInfo);
+    }
+  }
+
+  has(hasInfo) {
+    for (const item of this._datas) {
+      if (this.isEqual(item, hasInfo)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isEqual(data1, data2) {
+    if (data1 === 0 && data2 === 0) {
+      return true;
+    }
+
+    return Object.is(data1, data2);
+  }
+
+  delete(deleteInfo) {
+    for (let i = 0; i < this._datas.length; i++) {
+      if (this.isEqual(deleteInfo, this._datas[i])) {
+        this._datas.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  forEach(callBack) {
+    for (const iterator of this._datas) {
+      callBack(iterator, iterator, this._datas);
+    }
+  }
+
+  *[Symbol.iterator]() {
+    for (const item of this._datas) {
+      yield item;
+    }
+  }
 }
 
+const s = new MySets();
+
+const s2 = new MySets([3, 4, 5, 6, 7, 4, 5, 6]);
